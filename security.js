e551832c66d00bb5b4ddb2e9adcbdcee441814ae
StaticMaster <gnaboys@gmail.com>
2022-06-28 18:06:17 -0400

    changes

--- security.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require ("bcrypt")
-
-const pw  = require ("supersecretpassword")
-
-bcrypt.hash(pw,6, (err, hashedPw) =>{
-console.log (`Password is ${pw}`)
-console.log (`Hashed Password is ${hashedPw}`)
-})
